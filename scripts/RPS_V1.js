fb4ac0420168d8c86c1a9030067fb5f7c0b79e5b
debdevs <debdevs97@gmail.com>
2022-11-06 01:06:32 -0500

    import js, restructure project
    placed js file in newly created scripts folder. Added prompt as input value in js script.

--- scripts/RPS_V1.js
+++ scripts/RPS_V1.js
@@ -2,29 +2,49 @@ function getComputerChoice() {
   var choiceArray = ['Rock', 'Paper', 'Scissors'];
   const randomElement =
     choiceArray[Math.floor(Math.random() * choiceArray.length)];
-  console.log(randomElement);
+
   return randomElement;
 }
 const playerSelection = 'scissors';
 const playerSelection1 = playerSelection.toLowerCase();
 const computerSelection = getComputerChoice();
+let winner = null;
 function playRound(playerSelection1, computerSelection) {
   if (playerSelection === 'rock' && computerSelection == 'Scissors') {
     console.log('You won! congrats');
+    winner = 'you';
   } else if (playerSelection1 === 'scissors' && computerSelection == 'Rock') {
     console.log('You lost. Try again');
+    winner = 'computer';
   } else if (playerSelection1 === 'paper' && computerSelection == 'Rock') {
     console.log('You won! congrats');
+    winner = 'you';
   } else if (playerSelection1 === 'rock' && computerSelection == 'Paper') {
     console.log('You lost. Try again');
+    winner = 'computer';
   } else if (playerSelection1 === 'paper' && computerSelection == 'Scissors') {
     console.log('You lost. Try again');
+    winner = 'computer';
   } else if (playerSelection1 === 'scissors' && computerSelection == 'Paper') {
     console.log('You won! congrats');
+    winner = 'you';
   } else if (playerSelection1 === computerSelection.toLowerCase()) {
     console.log('Wow! You tied!');
+    winner = 'tie!';
   }
-  return playerSelection1;
+  return [playerSelection1, computerSelection, winner];
+}
+function game() {
+  for (let i = 0; i < 5; i++) {
+    console.log('NEW');
+    console.log(i);
+    let values = playRound(playerSelection1, computerSelection);
+    console.log(values);
+    const firstName = values[0],
+      lastName = values[1],
+      winner = values[2];
+  }
+  return winner;
 }
 
-console.log(playRound(playerSelection1, computerSelection));
+console.log(game(winner));
---
+++ scripts/rps.js
@@ -0,0 +1,72 @@
+function getComputerChoice() {
+  var choiceArray = ['Rock', 'Paper', 'Scissors'];
+  const randomElement =
+    choiceArray[Math.floor(Math.random() * choiceArray.length)];
+
+  return randomElement;
+}
+const playerSelection = 'scissors';
+const playerSelection1 = playerSelection.toLowerCase();
+const computerSelection = getComputerChoice();
+let winner = null;
+function playRound(playerSelection1, computerSelection) {
+  if (playerSelection === 'rock' && computerSelection == 'Scissors') {
+    console.log('You won! congrats');
+    winner = 'you';
+  } else if (playerSelection1 === 'scissors' && computerSelection == 'Rock') {
+    console.log('You lost. Try again');
+    winner = 'computer';
+  } else if (playerSelection1 === 'paper' && computerSelection == 'Rock') {
+    console.log('You won! congrats');
+    winner = 'you';
+  } else if (playerSelection1 === 'rock' && computerSelection == 'Paper') {
+    console.log('You lost. Try again');
+    winner = 'computer';
+  } else if (playerSelection1 === 'paper' && computerSelection == 'Scissors') {
+    console.log('You lost. Try again');
+    winner = 'computer';
+  } else if (playerSelection1 === 'scissors' && computerSelection == 'Paper') {
+    console.log('You won! congrats');
+    winner = 'you';
+  } else if (playerSelection1 === computerSelection.toLowerCase()) {
+    console.log('Wow! You tied!');
+    winner = 'tie!';
+  }
+  return [playerSelection1, computerSelection, winner];
+}
+
+function game() {
+  const winner_calc = [];
+  let player_wins = 0;
+  let computer_wins = 0;
+  for (let i = 0; i < 5; i++) {
+    console.log('NEW');
+    console.log(i);
+    // const playerSelection = document.getElementById('input').value;
+    let playerSelection = prompt('Please enter your choice', '');
+    const playerSelection1 = playerSelection.toLowerCase();
+    const computerSelection = getComputerChoice();
+
+    let values = playRound(playerSelection1, computerSelection);
+
+    const player = values[0],
+      computer = values[1],
+      winner = values[2];
+    winner_calc.push(winner);
+
+    if (winner === 'computer') {
+      computer_wins++;
+    } else if (winner === 'you') {
+      player_wins++;
+    } else {
+      console.log('TIE');
+    }
+
+    console.log(player, computer, winner);
+    console.log('Player Wins: ' + player_wins);
+    console.log('Computer Wins: ' + computer_wins);
+  }
+  return winner;
+}
+
+console.log(game(winner));
