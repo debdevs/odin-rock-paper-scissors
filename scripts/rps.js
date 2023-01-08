2d26ed7d4c43be361938183ac08bda58aa0c0a15
debdevs <deborahmdaniels1@gmail.com>
2023-01-07 23:29:03 -0500

    add display elements for game
    added display elements to display results

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 function getComputerChoice() {
   var choiceArray = ['Rock', 'Paper', 'Scissors'];
   const randomElement =
@@ -7,9 +13,10 @@ function getComputerChoice() {
 }
 const playerSelection = 'scissors';
 const playerSelection1 = playerSelection.toLowerCase();
-const computerSelection = getComputerChoice();
+
 let winner = null;
-function playRound(playerSelection1, computerSelection) {
+function playRound(playerSelection1) {
+  const computerSelection = getComputerChoice();
   if (playerSelection === 'rock' && computerSelection == 'Scissors') {
     console.log('You won! congrats');
     winner = 'you';
@@ -32,46 +39,70 @@ function playRound(playerSelection1, computerSelection) {
     console.log('Wow! You tied!');
     winner = 'tie!';
   }
+
+  document.getElementById("pc").innerHTML = "Rock";
+
+
+  const container = document.querySelector('#container');
+  const player_choice = document.createElement('div');
+  player_choice.classList.add('player_choice');
+  player_choice.textContent = "Player Choice:" + playerSelection1;
+  container.appendChild(player_choice);
+
+
+  const computer_choice = document.createElement('div');
+  computer_choice.classList.add('computer_choice');
+  computer_choice.textContent = "Computer Choice:" + computerSelection;
+  container.appendChild(computer_choice);
+
+
+
+  const winner_display = document.createElement('div');
+  winner_display.classList.add('winner_display');
+  winner_display.textContent = "Winner: " + winner;
+  container.appendChild(winner_display);
+  
   return [playerSelection1, computerSelection, winner];
 }
 
-function game() {
-  const winner_calc = [];
-  let player_wins = 0;
-  let computer_wins = 0;
-  for (let i = 0; i < 5; i++) {
-    console.log('NEW');
-    console.log(i);
-    // const playerSelection = document.getElementById('input').value;
-    let playerSelection = prompt('Please enter your choice', '');
-    const playerSelection1 = playerSelection.toLowerCase();
-    const computerSelection = getComputerChoice();
 
-    let values = playRound(playerSelection1, computerSelection);
+// function game() {
+//   const winner_calc = [];
+//   let player_wins = 0;
+//   let computer_wins = 0;
+//   for (let i = 0; i < 5; i++) {
+//     console.log('NEW');
+//     console.log(i);
+//     // const playerSelection = document.getElementById('input').value;
+//     let playerSelection = prompt('Please enter your choice', '');
+//     const playerSelection1 = playerSelection.toLowerCase();
+//     const computerSelection = getComputerChoice();
 
-    const player = values[0],
-      computer = values[1],
-      winner = values[2];
-    winner_calc.push(winner);
+//     let values = playRound(playerSelection1, computerSelection);
 
-    if (winner === 'computer') {
-      computer_wins++;
-    } else if (winner === 'you') {
-      player_wins++;
-    } else {
-      console.log('TIE');
-    }
+//     const player = values[0],
+//       computer = values[1],
+//       winner = values[2];
+//     winner_calc.push(winner);
 
-    console.log(player, computer, winner);
-    console.log('Player Wins: ' + player_wins);
-    console.log('Computer Wins: ' + computer_wins);
-    if (i >= 4 && player_wins > computer_wins) {
-      console.log('ROUND WINNER IS PLAYER');
-    } else if (i >= 4 && player_wins < computer_wins) {
-      console.log('ROUND WINNER IS COMPUTER');
-    }
-  }
-  return winner;
-}
+//     if (winner === 'computer') {
+//       computer_wins++;
+//     } else if (winner === 'you') {
+//       player_wins++;
+//     } else {
+//       console.log('TIE');
+//     }
 
-console.log(game(winner));
+//     console.log(player, computer, winner);
+//     console.log('Player Wins: ' + player_wins);
+//     console.log('Computer Wins: ' + computer_wins);
+//     if (i >= 4 && player_wins > computer_wins) {
+//       console.log('ROUND WINNER IS PLAYER');
+//     } else if (i >= 4 && player_wins < computer_wins) {
+//       console.log('ROUND WINNER IS COMPUTER');
+//     }
+//   }
+//   return winner;
+// }
+
+//console.log(game(winner));
